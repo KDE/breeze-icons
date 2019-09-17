7eae24228f355a09b4bfaa1579bef7244a9dfb21
Fabian Riethmayer <fabian@web2.0-apps.de>
2019-09-17 09:41:24 +0200

    Added scripts to create a webfont out of all breeze action icons
    Summary: Added a grunt task for generating (web)fonts out of the breeze action svg icons
    
    Reviewers: ndavis, ognarb
    
    Reviewed By: ndavis, ognarb
    
    Subscribers: ognarb, kde-frameworks-devel
    
    Tags: #frameworks
    
    Differential Revision: https://phabricator.kde.org/D24015

---
+++ webfont/Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        // Create the webfont files:
+        webfont: {
+                icons: {
+                        src: '../icons/actions/22@2x/*.svg',
+                        dest: './dist',
+                        options: {
+                            stylesheet: 'scss',
+                            types:  'eot,woff2,ttf,svg',
+                            optimize: false
+                        }
+                }
+        }
+    });
+    grunt.loadNpmTasks('grunt-webfont');
+    
+    // Run the default task by executing "grunt" in the CLI:
+    grunt.registerTask('default', ['webfont']);
+};
+
