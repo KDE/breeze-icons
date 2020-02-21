d0582a4f16bdf3947a085c31c5a0136e6283e7a1
Nate Graham <nate@kde.org>
2020-02-21 14:52:55 -0700

    Auto-generate 24px monochrome icons
    Summary:
    Right now we have 24px monochrome icons for compatibility with GTK apps, which use icons
    of this size. However they are visually identical to the 22px versions, with one pixel of
    padding on all sides. Manually creating them is tedious busywork.
    
    This patch removes all existing 24px monochrome icons and automatically generates them
    from the 22px versions for action, device, places, and status icons by editing the SVG
    files to increase the size of the viewbox and apply a (1,1) translation to the content.
    
    That way I implemented this seems to work pretty reliably on the icon generation side.
    However I am less sure about how it is integrated into the CMake files. It works, but if
    I did it wrong, let me know.
    
    Test Plan:
    Delete `~/kde/usr/share/icons/{breeze,breeze-dark}` (or wherever your icons live)
    Apply patch and build
    Look in `~/kde/usr/share/icons/{breeze,breeze-dark}/{devices,actions,places,status}/24`
    Open Cuttlefish
    
    See that everything is awesome
    
    Reviewers: #vdg, ndavis, #frameworks, sitter
    
    Reviewed By: #vdg, ndavis
    
    Subscribers: kde-frameworks-devel
    
    Tags: #frameworks
    
    Maniphest Tasks: T11950
    
    Differential Revision: https://phabricator.kde.org/D27557

---
+++ generate-24px-versions.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+OUTPUT_DIR=$1
+
+for INPUT_FILE in {devices,actions,places,status}/22/*.svg; do
+    OUTPUT_FILE="$OUTPUT_DIR/${INPUT_FILE/22/24}"
+    if [ ! -d $(dirname $OUTPUT_FILE) ] ; then
+        mkdir -p $(dirname $OUTPUT_FILE)
+    fi
+
+    # It's a symlink to another icon; make a link to mirror the one for the 22px version
+    if [ -L $INPUT_FILE ] ; then
+        LINK_TARGET=$(readlink $INPUT_FILE)
+        ln -fsn $LINK_TARGET $OUTPUT_FILE
+
+    # It's a file; make a 24px version of it
+    else
+        cp $INPUT_FILE $OUTPUT_FILE
+
+        # Change the size of the bounding box
+        if grep --quiet '"0 0 22 22' $OUTPUT_FILE; then
+            sed -i 's/"0 0 22 22/"0 0 24 24/g' $OUTPUT_FILE
+        # YOU
+        elif grep --quiet 'width="22"' $OUTPUT_FILE; then
+            sed -i 's/width="22"/width="24"/g' $OUTPUT_FILE
+            sed -i 's/height="22"/height="24"/g' $OUTPUT_FILE
+        else
+            echo $INPUT_FILE does not have the size defined in a parse-able way
+            exit 1
+        fi
+
+        # Group all the paths and transform them to fit in the center of a 24px box
+        if grep --quiet "</defs>" $OUTPUT_FILE; then
+            sed -i 's|</defs>|</defs><g transform="translate(1,1)">|g' $OUTPUT_FILE
+        elif grep --quiet "</style>" $OUTPUT_FILE; then
+            sed -i 's|</style>|</style><g transform="translate(1,1)">|g' $OUTPUT_FILE
+        else
+        # No stylesheet or defs tag; put the group at the end of the opening svg tag
+            sed -i 's|<svg \(.*\)>|<svg \1><g transform="translate(1,1)">|g' $OUTPUT_FILE
+        fi
+
+        # Add the closing group tag
+        sed -i 's|</svg>|</g></svg>|g' $OUTPUT_FILE
+    fi
+done
