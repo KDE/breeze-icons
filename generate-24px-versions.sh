542453b1df0e9d2f4bc1637eeaefd4bd356c07f9
Harald Sitter <sitter@kde.org>
2020-02-24 13:29:41 +0100

    make sed calls portable
    POSIX arg handling is a shambles and as a result an in-place call on bsd
    actually looks different to one on gnu. use a hacky way to bypass this
    by checking the help output and define an appropriate wrapper function

--- generate-24px-versions.sh
+++ generate-24px-versions.sh
@@ -4,6 +4,22 @@ set -e
 
 OUTPUT_DIR=$1
 
+# Compat between freebsd and gnu.
+case $(sed --help 2>&1) in
+    *GNU*)
+        echo "Using GNU-style sed arguments"
+        sed_i () {
+            sed -i "$@"
+        }
+        ;;
+    *)
+        echo "Using POSIX-style sed arguments"
+        sed_i () {
+            sed -i '' "$@"
+        }
+        ;;
+esac
+
 for INPUT_FILE in {devices,actions,places,status}/22/*.svg; do
     OUTPUT_FILE="$OUTPUT_DIR/${INPUT_FILE/22/24}"
     if [ ! -d $(dirname $OUTPUT_FILE) ] ; then
@@ -21,11 +37,11 @@ for INPUT_FILE in {devices,actions,places,status}/22/*.svg; do
 
         # Change the size of the bounding box
         if grep --quiet '"0 0 22 22' $OUTPUT_FILE; then
-            sed -i 's/"0 0 22 22/"0 0 24 24/g' $OUTPUT_FILE
+            sed_i -e 's/"0 0 22 22/"0 0 24 24/g' $OUTPUT_FILE
         # YOU
         elif grep --quiet 'width="22"' $OUTPUT_FILE; then
-            sed -i 's/width="22"/width="24"/g' $OUTPUT_FILE
-            sed -i 's/height="22"/height="24"/g' $OUTPUT_FILE
+            sed_i -e 's/width="22"/width="24"/g' $OUTPUT_FILE
+            sed_i -e 's/height="22"/height="24"/g' $OUTPUT_FILE
         else
             echo $INPUT_FILE does not have the size defined in a parse-able way
             exit 1
@@ -33,16 +49,16 @@ for INPUT_FILE in {devices,actions,places,status}/22/*.svg; do
 
         # Group all the paths and transform them to fit in the center of a 24px box
         if grep --quiet "</defs>" $OUTPUT_FILE; then
-            sed -i 's|</defs>|</defs><g transform="translate(1,1)">|g' $OUTPUT_FILE
+            sed_i -e 's|</defs>|</defs><g transform="translate(1,1)">|g' $OUTPUT_FILE
         elif grep --quiet "</style>" $OUTPUT_FILE; then
-            sed -i 's|</style>|</style><g transform="translate(1,1)">|g' $OUTPUT_FILE
+            sed_i -e 's|</style>|</style><g transform="translate(1,1)">|g' $OUTPUT_FILE
         else
         # No stylesheet or defs tag; put the group at the end of the opening svg tag
-            sed -i 's|<svg \(.*\)>|<svg \1><g transform="translate(1,1)">|g' $OUTPUT_FILE
+            sed_i -e 's|<svg \(.*\)>|<svg \1><g transform="translate(1,1)">|g' $OUTPUT_FILE
         fi
 
         # Add the closing group tag
-        sed -i 's|</svg>|</g></svg>|g' $OUTPUT_FILE
+        sed_i -e 's|</svg>|</g></svg>|g' $OUTPUT_FILE
     fi
 done
 
