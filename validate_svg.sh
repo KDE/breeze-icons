def21bf6c691a40bef233efd898fcb7ff57d55bb
Pino Toscano <pino@kde.org>
2020-04-16 11:47:02 +0200

    build: quote source directory path
    This way it will work even if the path of the source directory contains
    special characters, like whitespaces.
    
    Fixes commit 0a5dd2972b62d391d0d7176c2748653dbe3e110a.

--- validate_svg.sh
+++ validate_svg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-find ${1:-.} -type f -name '*.svg' -exec xmllint --noout {} + 2> xmlerrors
+find "${1:-.}" -type f -name '*.svg' -exec xmllint --noout {} + 2> xmlerrors
 if [ -s xmlerrors ]; then
     cat xmlerrors
     rm xmlerrors
