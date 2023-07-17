011d501490c66da57ae81cdf5a1812bb4030159f
Christoph Cullmann <cullmann@kde.org>
2023-07-17 13:56:44 +0200

    only bundle normal theme and set fallback theme

--- src/breezeicons.cpp
+++ src/breezeicons.cpp
@@ -8,53 +8,15 @@
 
 #include "breezeicons.h"
 
-#include <QGuiApplication>
 #include <QIcon>
-#include <QPalette>
-
-/**
- * Set right theme for given palette
- * @param palette palette to derive theme from
- */
-static void updateThemeForPalette(const QPalette &palette)
-{
-    // decide if we want normal or dark theme based on application palette
-    const auto darkTheme = palette.color(QPalette::Base).lightness() < 128;
-    const auto themeName = darkTheme ? QStringLiteral("breeze-dark") : QStringLiteral("breeze");
-
-    // enforce theme + fallback theme
-    QIcon::setFallbackThemeName(themeName);
-    QIcon::setThemeName(themeName);
-}
-
-// stored connection for later disconnect in deinitIcons
-Q_GLOBAL_STATIC(QMetaObject::Connection, updateConnection)
 
 namespace BreezeIcons
 {
 
 void initIcons()
 {
-    // we require a gui application instance
-    Q_ASSERT(qGuiApp);
-
-    // once update theme manually
-    updateThemeForPalette(qGuiApp->palette());
-    printf("lslslslslslslsafn\n");
-
-    // register for later changes
-    *updateConnection() = QObject::connect(qGuiApp, &QGuiApplication::paletteChanged, &updateThemeForPalette);
-}
-
-void deinitIcons()
-{
-    // if we have no gui application or no connection setup => nop
-    if (!qGuiApp || !(*updateConnection())) {
-        return;
-    }
-
-    // unregister theme update function to allow again other themes to be used without interference
-    QObject::disconnect(*updateConnection());
+    // ensure we fallback to breeze
+    QIcon::setFallbackThemeName(QStringLiteral("breeze"));
 }
 
 }
--- src/breezeicons.h
+++ src/breezeicons.h
@@ -19,31 +19,12 @@ namespace BreezeIcons
 {
 
 /**
- * Sets up the Breeze icons and sets the icon theme used by Qt
+ * Sets up the Breeze icons and sets the fallback icon theme used by Qt
  * to the Breeze icons.
  *
- * This automatically switches between Breeze light and Breeze dark
- * based on the base color of your application's palette, and listens
- * to changes to the application's palette and switches between the
- * icon themes accordingly.
- *
- * Call this function after having constructed your QGuiApplication or
- * QApplication.
- *
- * @warning Calling this function before a QGuiApplication has been constructed
- * will likely cause a crash.
- *
- * @since 5.80
+ * @since 6.0
  */
 BREEZEICONS_EXPORT void initIcons();
-
-/**
- * Deactivates the automatic switching between Breeze light and Breeze dark
- * as activated with initIcons again.
- *
- * @since 5.80
- */
-BREEZEICONS_EXPORT void deinitIcons();
 
 }
 
