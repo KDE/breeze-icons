b5a8d4db806d1ce8d11aadebbe6dc7604fc63bc9
Carl Schwan <carl@carlschwan.eu>
2023-08-29 18:27:23 +0000

    Apply 10 suggestion(s) to 1 file(s)

--- subsetBinaryIcons.cpp
+++ subsetBinaryIcons.cpp
@@ -1,23 +1,8 @@
-/*  This file is part of the KDE libraries
- *    Copyright (C) 2016 Kåre Särs <kare.sars@iki.fi>
- *
- *     SPDX-FileCopyrightText: 2023 g10 Code GmbH
- *     SPDX-FileContributor: Sune Stolborg Vuorela <sune@vuorela.dk>
- *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Library General Public
- *    License as published by the Free Software Foundation; either
- *    version 2 of the License, or (at your option) any later version.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Library General Public License for more details.
- *
- *    You should have received a copy of the GNU Library General Public License
- *    along with this library; see the file COPYING.LIB.  If not, write to
- *    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- *    Boston, MA 02110-1301, USA.
+/* This file is part of the KDE libraries
+ * SPDX-FileCopyrightText: 2016 Kåre Särs <kare.sars@iki.fi>
+ * SPDX-FileCopyrightText: 2023 g10 Code GmbH
+ * SPDX-FileContributor: Sune Stolborg Vuorela <sune@vuorela.dk>
+ * SPDX-License-Identifier: LGPL-2.0-or-later
  */
 #include <QCommandLineParser>
 #include <QCoreApplication>
@@ -31,27 +16,27 @@ int parseFile(const QString &infile, const QString &outfile, const QStringList &
 {
     QFile in(infile);
     QFile out(outfile);
-    QRegularExpression imageReg(QStringLiteral("<file>(.*\\.(?:svg|png|gif|ico))</file>"));
+    static QRegularExpression imageReg(QStringLiteral("<file>(.*\\.(?:svg|png|gif|ico))</file>"));
 
     if (!in.open(QIODevice::ReadOnly)) {
-        qDebug() << "Failed to open" << infile;
+        qWarning() << "Failed to open" << infile;
         return -1;
     }
     if (!out.open(QIODevice::WriteOnly)) {
-        qDebug() << "Failed to create" << outfile;
+        qWarning() << "Failed to create" << outfile;
         return -2;
     }
 
     while (in.bytesAvailable()) {
-        QString line = QString::fromLocal8Bit(in.readLine());
-        QRegularExpressionMatch match = imageReg.match(line);
+        const QString line = QString::fromLocal8Bit(in.readLine());
+        const QRegularExpressionMatch match = imageReg.match(line);
         if (!match.hasMatch()) {
             // qDebug() << "No Match: " << line;
             out.write(qPrintable(line));
             continue;
         }
 
-        QFileInfo info(match.captured(1));
+        const QFileInfo info(match.captured(1));
         if (!includeFilter.contains(info.completeBaseName())) {
             continue;
         }
@@ -65,12 +50,12 @@ static QStringList parseIncludeList(const QString &path)
     QStringList items;
     QFile in(path);
     if (!in.open(QIODevice::ReadOnly)) {
-        qDebug() << "failed to open" << path;
+        qWarning() << "failed to open" << path;
         return {};
     }
     while (in.bytesAvailable()) {
-        QString line = QString::fromLocal8Bit(in.readLine()).trimmed();
-        if (line.startsWith(QStringLiteral("#"))) {
+        const QString line = QString::fromLocal8Bit(in.readLine()).trimmed();
+        if (line.startsWith(QLatin1Char('#'))) {
             continue;
         }
         items.push_back(line);
@@ -84,9 +69,9 @@ int main(int argc, char *argv[])
 
     QCommandLineParser parser;
 
-    QCommandLineOption inOption(QStringList() << QLatin1String("i") << QLatin1String("infile"), QStringLiteral("Input qrc file"), QStringLiteral("infile"));
-    QCommandLineOption outOption(QStringList() << QLatin1String("o") << QLatin1String("outfile"), QStringLiteral("Output qrc file"), QStringLiteral("outfile"));
-    QCommandLineOption includeOption(QStringList() << QLatin1String("I") << QLatin1String("include"),
+    QCommandLineOption inOption({ QLatin1String("i"), QLatin1String("infile") }, QStringLiteral("Input qrc file"), QStringLiteral("infile"));
+    QCommandLineOption outOption({ QLatin1String("o"), QLatin1String("outfile") }, QStringLiteral("Output qrc file"), QStringLiteral("outfile"));
+    QCommandLineOption includeOption({ QLatin1String("I"), QLatin1String("include") },
                                      QStringLiteral("InputFilter list (newline delimited, no extensions)"),
                                      QStringLiteral("include"));
     parser.setApplicationDescription(
